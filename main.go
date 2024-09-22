f0afdf2ddc8ddf5360645450f808f748cab539fc
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2024-09-22 17:40:06 -0400

    set up basic routes

--- main.go
+++ main.go
@@ -3,21 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	r := chi.NewRouter()
+
+	port := ":3000"
 	server := &http.Server{
-		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Addr:    port,
+		Handler: r,
 	}
+
+	r.Get("/get", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("get route called")
+	})
+
+	r.Post("/upload", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("upload route called")
+	})
 
 	err := server.ListenAndServe()
 
 	if err != nil {
 		fmt.Println("failed to listen to server", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
 }
